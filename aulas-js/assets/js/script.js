f09b844e451eb1bf26feeef66381deca3fac82ef
victor 406 <victor.ezaki@alunos.sc.senac.br>
2025-10-07 22:01:08 -0300

    mais exercicio

--- aulas-js/assets/js/script.js
+++ aulas-js/assets/js/script.js
@@ -1,12 +1,36 @@
 const btn = document.querySelector("#calcular");
 const camposAltura = document.querySelectorAll(".alturas");
-const camposSexo = document.getElementsByName("sexo");
+const camposGenero = document.getElementsByName("genero");
 
-let pessoa = {}
+const genero1 = document.querySelector('input[name="genero1"]:checked');
+
+if (genero1) {
+  console.log(genero1.value); // "f" ou "m"
+} else {
+  console.log("Nenhum gênero selecionado para Pessoa 1");
+}
+
+
+let alturas = []
+let genero = {}
 
 
 btn.addEventListener('click', () => {
-    campos.forEach(campo => {
+    camposAltura.forEach(campo => {
         alturas.push(campo.value);
     })
+
+    camposGenero.forEach(campo => {
+        genero.push(campo.value);
+    })
+
+    let qtdeHomens = 0;
+
+    for (let i = 0; i < genero.length; i++) {
+        if (genero[i] == "m") {
+            qtdeHomens++;
+        }
+    }
+
+    console.log(qtdeHomens);
 });
